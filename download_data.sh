16e23da244540f842aedc17d756ef173847529d4
Shyam Sundar <shyamsravikumar@gmail.com>
2020-11-22 21:52:56 -0800

    Added data extraction script

---
+++ download_data.sh
@@ -0,0 +1,4 @@
+wget https://www.dropbox.com/s/ppkk2n1kyqj5izq/yelp_data.tar.gz?dl=0
+mv yelp_data.tar.gz\?dl=0 models.tar.gz
+tar -xvzf yelp_data.tar.gz
+rm -rf yelp_data.tar.gz data/yelp_data.tar.gz
